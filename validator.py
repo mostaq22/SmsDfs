167d086dc51224207f1dae9564bcb72a9a161707
Mostaq Mahmud <mostaqcse@gmail.com>
2022-06-05 19:01:56 +0600

    Update validator.py
    validation added

--- validator.py
+++ validator.py
@@ -1,3 +1,5 @@
+MNO_CODE = ["018", "011", "017", "011", "019", "012"]  # Will be Read from env file
+
 """
 Rules of validation of a mobile number
 
@@ -9,12 +11,25 @@ Rules of validation of a mobile number
 
 
 def is_valid_mobile_number(mobile_number: str):
-    print(mobile_number, type(mobile_number))
-    if mobile_number.startswith('01') and len(mobile_number):
-        pass
-    elif mobile_number[:]:
-        pass
+    if len(mobile_number) != 11:
+        print("length is not 11")
+    elif not mobile_number.startswith('01'):
+        print("not start with 01")
+    elif not type(mobile_number) is str:
+        print("not string")
+    elif mobile_number[-11:-8] in MNO_CODE:
+        print("MNO code invalid")
+
+
+# try:
+#     if len(mobile_number) == 11 and \
+#             mobile_number.startswith('01') and \
+#             type(mobile_number) is str and \
+#             mobile_number[-11:-8] in MNO_CODE:
+#         return True
+# finally:
+#     raise Exception("Mobile number is not valid")
 
 
 if __name__ == '__main__':
-    is_valid_mobile_number(71813208359)
+    is_valid_mobile_number('')
