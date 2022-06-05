cc7efdbf56f491a6434ff93571b2566004de843c
Mostaq Mahmud <mostaqcse@gmail.com>
2022-06-05 13:16:56 +0600

    Update Sms.py
    mobile number validation, MNO validation, MNO code set-get

--- Sms.py
+++ Sms.py
@@ -1,5 +1,16 @@
 from typing import Union
 
+"""
+Class Name: SmsEngine
+Description: The SmsEngine class will take some param during class initialization.
+    Based on the init params this class will generate & validate some properties like MNO, mobile number etc.
+    For future new MNO integration the __mno_list properties will be updated
+params: 
+    - mobile
+    - message
+    - mno    
+"""
+
 
 class SmsEngine:
     __mobile_number: str
@@ -9,31 +20,33 @@ class SmsEngine:
         "GP": ["013", "017"],  # GrameenPhone
         "RB": ["016", "018"],  # Robi 018 - 1320859
         "BL": ["014", "019"],  # Banglalink
-        "SS": []
     }  # The new MNO will be added based on new integration
     __priority = ('OTP', 'CNF', 'GRL')  # Base on the priority the sms will be disbursed.
     __default_mno = None
-    __operator_code: Union[str, None] = None
-    MOBILE_NUMBER_LENGTH = 11
+    __mno_code: Union[str, None] = None
 
     def __init__(self, mobile_number: str, message: str, mno: Union[str, None] = None,
                  priority: Union[str, None] = 'GRL'):
         self.__mobile_number = mobile_number
         self.__message = message
         self.__mno = mno
-        self.__priority = priority
+        self.__priority = priority if priority in self.__priority else 'GRL'
         self.is_valid()
-
-        self.__operator_code = self.__mobile_number[:2]
 
     def get_mno_list(self):
         return self.__mno_list.keys()
+
+    """
+    The is_valid function is responsible to validate following attributes
+    * Mobile number
+    * Provided MNO
+    """
 
     # validate class related attributes
     def is_valid(self) -> bool:
         try:
             self.validate_mobile_number()
-            # self.validate_mno()
+            self.validate_mno()
             return True
         except TypeError:
             raise TypeError
@@ -44,39 +57,53 @@ class SmsEngine:
         pattern = re.compile(r"(^(\+8801|8801|01|008801))[1|3-9]{1}(\d){8}$")
 
         if re.match(pattern, self.__mobile_number):
-            self.__operator_code = self.__mobile_number[-11:-8]
             return True
         else:
             raise Exception("Invalid mobile number")
 
-    # validate mno
+    """
+    The validate_mno method is responsible to validate the provided MNO in class initialization.
+    * validate MNO
+    * set MNO - if validation fails
+    * set MNO code from mobile number
+    """
+
     def validate_mno(self) -> bool:
-        if self.__mno in self.__mno_list.keys():
+        if self.__mno is not None and self.__mno in self.__mno_list.keys():
+            self.__set_mno_code()
             return True
         else:
+            self.__set_mno_code()
             self.__set_mno()
-
-    def setup_mno(self):
-        pass
+            return True
 
     # set mno for a mobile number if the MNO validation is failed
     def __set_mno(self) -> None:
-        for mno, number in self.__mno_list:
-            self.__mno = mno if self.__operator_code in number else None
+        for key in self.__mno_list:
+            if self.__mno_code in self.__mno_list[key]:
+                self.__mno = key
 
+    # set MNO code from provided mobile number
+    def __set_mno_code(self) -> None:
+        self.__mno_code = self.__mobile_number[-11:-8]
 
     # get MNO for a mobile number
     @property
-    def get_mno(self) -> str:
+    def mno_code(self) -> str:
+        return self.__mno_code
+
+    # get MNO for a mobile number
+    @property
+    def mno(self) -> str:
         return self.__mno
 
     # get message
     @property
-    def get_message(self) -> str:
+    def message(self) -> str:
         return self.__message
 
     @property
-    def get_priority(self) -> str:
+    def priority(self) -> str:
         return self.__priority
 
     """
@@ -88,13 +115,13 @@ class SmsEngine:
 
 
 sms = SmsEngine(
-    mobile_number="+8801716253638",
+    mobile_number="+8801816253638",
     message="Hi!",
     mno='NL',
-    priority='SSS'
+    priority='OTP'
 )
 print(
     sms.send(),
-    sms.get_priority,
-    sms.get_mno,
+    sms.priority,
+    sms.mno,
 )
